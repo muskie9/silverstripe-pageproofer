860edb6139f0822252262331244ea3b92840f671
muskie9 <nhorstmeier@gmail.com>
2016-02-07 13:25:56 -0600

    REFACTOR tests to increase code coverage

--- tests/PageProoferConfig_Test.php
+++ tests/PageProoferConfig_Test.php
@@ -14,4 +14,16 @@ class PageProoferConfig_Test extends PP_Test
         $this->assertTrue(PageProoferConfig::get_page_proofer()->ID != 0);
     }
 
+    /**
+     * Test that our PageProoferCodes GridField exists in SiteConfig
+     */
+    public function testSiteConfigFields()
+    {
+        $siteConfig = singleton('SiteConfig');
+        $fields = $siteConfig->getCMSFields();
+
+        $this->assertTrue($fields->fieldPosition('PageProoferCodes') != false);
+
+    }
+
 }
